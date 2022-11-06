df849bc27a6609fafc33d75eb13f8fd7ca291214
georgemwangi118 <gmwangi118@yahoo.com>
2022-11-06 13:04:42 +0300

    initial commit

---
+++ server/server.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const path = require("path");
+
+const app = express();
+
+app.use("/", express.static(path.join(__dirname, "/public")));
+
+app.use("/", require("./routes/root"));
+
+app.all("*", (req, res) => {
+  res.status(404);
+  if (req.accepts("html")) {
+    res.sendFile(path.join(__dirname, "views", "404.html"));
+  } else if (req.accepts("json")) {
+    res.json({ message: "404 Not Found" });
+  } else {
+    res.type("txt").send("404 Not Found");
+  }
+});
+
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => console.log(`Server running on port ${port}`));
