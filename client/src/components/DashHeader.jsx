6411af26ba6087bbbd81df798d8a19ff90bc58d8
georgemwangi118 <gmwangi118@yahoo.com>
2022-11-07 16:01:16 +0300

    dashboard

--- client/src/components/DashHeader.jsx
+++ client/src/components/DashHeader.jsx
@@ -1,7 +1,17 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const DashHeader = () => {
-  return <div>DashHeader</div>;
+  return (
+    <header className="dash-header">
+      <div className="dash-header__container">
+        <Link>
+          <h1 className="dash-header__title">techNotes</h1>
+        </Link>
+        <nav className="dash-header__nav"></nav>
+      </div>
+    </header>
+  );
 };
 
 export default DashHeader;
--- client/src/components/DashLayout.jsx
+++ client/src/components/DashLayout.jsx
@@ -1,7 +1,18 @@
 import React from "react";
+import { Outlet } from "react-router-dom";
+import DashFooter from "./DashFooter";
+import DashHeader from "./DashHeader";
 
 const DashLayout = () => {
-  return <div>DashLayout</div>;
+  return (
+    <>
+      <DashHeader />
+      <div className="dash-container">
+        <Outlet />
+      </div>
+      <DashFooter />
+    </>
+  );
 };
 
 export default DashLayout;
